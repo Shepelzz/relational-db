9176c1d391d56ef33b178934404f12e178c55d6c
Zhenya_home <shepelzz@gmail.com>
2018-09-01 13:02:17 +0300

    db

--- src/lesson2/Demo.java
+++ src/lesson2/Demo.java
@@ -1,16 +1,21 @@
 package lesson2;
 
+import java.util.Date;
+
 public class Demo {
     public static void main(String[] args) {
         //java
-            //create class
-            //create object with val
-            //using it
+            //create class +
+            //create object with val +
+            //using it +
 
+        Order order = new Order(1001,"tool2212", 140, new Date(), new Date());
 
         //db
-            //create tbl
-            //insert some data
-            //using it
+            //create tbl +
+            //insert some data +
+            //using it +
+
+
     }
 }
