d8643f85108db93511c7d3e92bc5a106e70feff7
Zhenya_home <shepelzz@gmail.com>
2018-09-01 12:30:29 +0300

    db

---
+++ src/lesson2/Demo.java
@@ -0,0 +1,16 @@
+package lesson2;
+
+public class Demo {
+    public static void main(String[] args) {
+        //java
+            //create class
+            //create object with val
+            //using it
+
+
+        //db
+            //create tbl
+            //insert some data
+            //using it
+    }
+}
